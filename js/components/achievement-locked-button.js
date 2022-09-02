159fb00fb26412e5ae12eaa67769d7505082357a
sussy layers dev <72522395+jwklong@users.noreply.github.com>
2022-09-02 16:05:07 +0100

    Update achievement-locked-button.js

--- js/components/achievement-locked-button.js
+++ js/components/achievement-locked-button.js
@@ -1,10 +1,10 @@
 Vue.component("achievement-locked-button", {
     props: ["achievements"],
     methods: {
-        completedAchievements: () => game.achievements.filter(ach => ach.isCompleted)
+        completedAchievements: () => game.achievements.filter(ach => ach.length)
     },
     template: `<button @click="$emit('click')" :disabled="completedAchievements() < achievements">
     <span v-if="completedAchievements() < achievements">Reach {{achievements}}<img alt="LC" class="inline" src="images/star.svg"/></span>
-    <span v-else><slot></slot>{{achievements}}{{completedAchievements()}}</span>
+    <span v-else><slot></slot></span>
 </button>`
 })
