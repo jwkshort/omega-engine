8aaa17d4399ef2d1f8a9390fa4b1a5e2fe3d1c88
also sussy layers dev <100470464+jwkshort@users.noreply.github.com>
2022-10-26 17:00:40 +0100

    Create functions-locked-button.js

---
+++ js/mods/functions-locked-button.js
@@ -0,0 +1,11 @@
+Vue.component("aleph-locked-button", {
+    props: ["functions"],
+    methods: {
+        totalFunctions: () => game.functionsLayer.functionsPoints,
+        Decimal: (x) => new Decimal(x)
+    },
+    template: `<button @click="$emit('click')" :disabled="Decimal(totalFunctions()).lt(functions)">
+    <span v-if="Decimal(totalFunctions()).lt(functions)">Reach {{functions}}<span class="aleph">ƒP</span></span>
+    <span v-else><slot></slot></span>
+</button>`
+})
